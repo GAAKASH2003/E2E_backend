4fa13a41505f3567c32eeb71c3543c3592c41f20
Gopu Aakash <gopuaakash751@gmail.com>
2025-07-18 21:50:36 +0530

    fix:nodemailerPort

--- utils/sendMail.ts
+++ utils/sendMail.ts
@@ -2,6 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 465,
+  host: "smtp.gmail.com",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
